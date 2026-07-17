39352a41668fd7e0f5c06c501c5e9e90ac57cfcb
sureshcherukuri <chsuresh61@gmail.com>
2026-07-17 20:12:51 +0530

    gradle modified

--- app/src/main/java/com/example/testingproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/presentation/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.testingproject.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            UserScreen()
+        }
     }
+}
+
+@Composable
+fun UserScreen(){
+
 }
