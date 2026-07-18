20b279f2c8b59df45e0673a450462c1827fcbd54
sureshcherukuri <chsuresh61@gmail.com>
2026-07-18 17:26:01 +0530

    test case

---
+++ app/src/main/java/com/example/testingproject/Calculator.kt
@@ -0,0 +1,8 @@
+package com.example.testingproject
+
+class Calculator {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
---
+++ app/src/test/java/com/example/testingproject/CalculatorTest.kt
@@ -0,0 +1,21 @@
+package com.example.testingproject
+
+import org.junit.Assert.*
+import org.junit.Before
+import kotlin.test.Test
+
+class CalculatorTest {
+
+    private lateinit var calculator: Calculator
+
+    @Before
+    fun start(){
+        calculator= Calculator()
+    }
+
+    @Test
+    fun check(){
+        val result=calculator.add(10,10)
+        assertEquals(30,result)
+    }
+}
